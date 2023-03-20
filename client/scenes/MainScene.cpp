959cdf88832beee133287667cc2cb12095002a8b
Paul COMTE <paul.codepro@gmail.com>
2023-03-20 01:45:12 +0100

    :art: beautify MainScene code

--- client/scenes/MainScene.cpp
+++ client/scenes/MainScene.cpp
@@ -11,16 +11,22 @@ MainScene::MainScene(std::shared_ptr<ClientManager> clientManager) {
     if (clientManager == nullptr)
         throw ClientError("Whilst initializing LoggingScene: ClientManager cannot be null !");
     this->_clientManager = clientManager;
-    this->_initWidgets();
+    this->_parent = std::shared_ptr<QWidget>(new QWidget(this->getSceneManager()->getWidget().get()));
+    this->_parent->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    this->_parent->setMinimumSize(1280, 720);
+
     this->_initLayouts();
-    this->_placeWidgets();
+    this->_placeLayouts();
     this->clear();
 }
 
 MainScene::~MainScene() {
+    this->_parentLayout.reset();
     this->_user.reset();
     this->_contacts.reset();
     this->_search.reset();
+    this->_leftSideLayout.reset();
+    this->_rightSideLayout.reset();
 }
 
 std::string MainScene::getName() {
@@ -55,26 +61,40 @@ std::shared_ptr<SceneManager> MainScene::getSceneManager() {
     return (this->_clientManager->sceneManager);
 }
 
-void MainScene::_initWidgets() {
-    this->_parent = std::shared_ptr<QWidget>(new QWidget(this->getSceneManager()->getWidget().get()));
-    this->_leftSide = std::shared_ptr<QWidget>(new QWidget(this->_parent.get()));
-    this->_parent->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    this->_leftSide->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    this->_parent->setMinimumSize(1280, 720);
+void MainScene::_initLayouts() {
+    this->_parentLayout = std::unique_ptr<QHBoxLayout>(new QHBoxLayout(this->_parent.get()));
+
+    this->_initLeftLayout();
+    this->_initRightLayout();
+}
+
+
+void MainScene::_initLeftLayout() {
+    this->_leftSideLayout = std::unique_ptr<QVBoxLayout>(new QVBoxLayout());
 
     this->_user = std::unique_ptr<UserScene>(new UserScene(this->_clientManager));
     this->_contacts = std::unique_ptr<ContactScene>(new ContactScene(this->_clientManager));
     this->_search = std::unique_ptr<SearchScene>(new SearchScene(this->_clientManager));
 }
 
-void MainScene::_initLayouts() {
-    this->_leftSideLayout = std::unique_ptr<QVBoxLayout>(new QVBoxLayout(this->_leftSide.get()));
+void MainScene::_initRightLayout() {
+    this->_rightSideLayout = std::unique_ptr<QVBoxLayout>(new QVBoxLayout());
 }
 
-void MainScene::_placeWidgets() {
+void MainScene::_placeLayouts() {
+    this->_parentLayout->addLayout(this->_leftSideLayout.get(), 2);
+    this->_parentLayout->addLayout(this->_rightSideLayout.get(), 5);
+
+    this->_placeLeftLayout();
+    this->_placeRightLayout();
+}
+
+void MainScene::_placeLeftLayout() {
     this->_user->getWidget()->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
     this->_leftSideLayout->addWidget(this->_user->getWidget().get());
     this->_leftSideLayout->addLayout(this->_search->getLayout().get());
     this->_leftSideLayout->addWidget(this->_contacts->getWidget().get());
-    this->_leftSide->setLayout(this->_leftSideLayout.get());
+}
+
+void MainScene::_placeRightLayout() {
 }
--- client/scenes/MainScene.hpp
+++ client/scenes/MainScene.hpp
@@ -21,18 +21,22 @@ namespace babel {
             std::shared_ptr<SceneManager> getSceneManager() override;
 
         private:
-            void _initWidgets();
             void _initLayouts();
-            void _placeWidgets();
-    
+            void _initLeftLayout();
+            void _initRightLayout();
+
+            void _placeLayouts();
+            void _placeLeftLayout();
+            void _placeRightLayout();
+
             std::shared_ptr<ClientManager> _clientManager = nullptr;
-    
             std::shared_ptr<QWidget> _parent = nullptr;
+            std::unique_ptr<QHBoxLayout> _parentLayout = nullptr;
             std::unique_ptr<UserScene> _user = nullptr;
             std::unique_ptr<ContactScene> _contacts = nullptr;
             std::unique_ptr<SearchScene> _search = nullptr;
-            std::shared_ptr<QWidget> _leftSide = nullptr;
             std::unique_ptr<QVBoxLayout> _leftSideLayout = nullptr;
+            std::unique_ptr<QVBoxLayout> _rightSideLayout = nullptr;
     };
 
 }
